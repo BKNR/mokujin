f3a700c22ed7b3b0c450be2003800dec5f882654
TLNBS2405 <33620644+TLNBS2405@users.noreply.github.com>
2019-11-16 10:47:51 +0100

    Update mokujin.py

--- mokujin.py
+++ mokujin.py
@@ -96,10 +96,10 @@ async def on_message(message):
     if I'm going to change it.
     '''
     channel = message.channel
-    if message.content.startswith('?') and ('frame' in channel.name):
+    if message.content.startswith('?') and (('tekken' in channel.name) or ('frame' in channel.name)):
 
         user_message = message.content
-        user_message = user_message.replace('?', '')
+        user_message = user_message.replace('!', '')
         user_message_list = user_message.split(' ', 1)
 
         if len(user_message_list) <= 1:
